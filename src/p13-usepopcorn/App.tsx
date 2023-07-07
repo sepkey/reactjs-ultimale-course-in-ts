00e31d6d2aae6d73295c010812a6fb021ee5ad98
Sepide Kia <sepidekia6990@gmail.com>
2023-07-07 15:46:51 +0330

    add useEffect's alternative, using an mouseEvent to handle first search

--- src/p13-usepopcorn/App.tsx
+++ src/p13-usepopcorn/App.tsx
@@ -98,6 +98,48 @@ export default function App() {
   //   };
   // }, [query]);
 
+  //alternative for useEffect
+  // function handleSearchFocus() {
+  //   const controller = new AbortController();
+
+  //   async function getMovies(query: string) {
+  //     try {
+  //       setIsloading(true);
+  //       setError("");
+  //       const response = await fetch(
+  //         `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`,
+  //         { signal: controller.signal }
+  //       );
+
+  //       if (!response.ok)
+  //         throw new Error("Something went wrong whith fetching movies!");
+  //       const data = await response.json();
+  //       if (data.Response === "False") throw new Error("Movie not found!");
+  //       const body = data.Search as unknown;
+  //       assertIsMovies(body);
+
+  //       setMovies(body);
+  //       setError("");
+  //     } catch (err: any) {
+  //       if (err.name !== "AbortError") {
+  //         console.log(err);
+  //         setError(err.message);
+  //       }
+  //     } finally {
+  //       setIsloading(false);
+  //     }
+  //   }
+
+  //   if (query.length < 3) {
+  //     setMovies([]);
+  //     setError("");
+  //     return;
+  //   }
+
+  //   handleCloseDetaild();
+  //   getMovies(query);
+  // }
+
   useEffect(
     function () {
       const controller = new AbortController();
