0d2559e0ca381f93608782a013eb4e89cdfe382d
Sepide Kia <sepidekia6990@gmail.com>
2023-07-08 22:59:49 +0330

    add focus using useRef

--- src/p15-usepopcorn/Search.tsx
+++ src/p15-usepopcorn/Search.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useRef } from "react";
+
 export type Props = {
   query: string;
   setQuery: React.Dispatch<React.SetStateAction<string>>;
 };
 export function Search({ query, setQuery }: Props) {
+  //how not to select Dom element
+  //if we have to add a dpendenccy array, it would select the element over and over again
+  // useEffect(function () {
+  //   const el = document.querySelector(".search") as HTMLInputElement;
+  //   el.focus();
+  // }, []);
+
+  const inputEl = useRef<HTMLInputElement>(null);
+  useEffect(function () {
+    inputEl.current?.focus();
+  }, []);
   return (
     <input
+      ref={inputEl}
       className="search"
       type="text"
       placeholder="Search movies..."
