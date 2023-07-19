1d76c65bdd43d3348bccc0743422742ff852d3ab
Sepide Kia <sepidekia6990@gmail.com>
2023-07-19 23:39:24 +0330

    add basic router

--- src/p21-worldwise/App.tsx
+++ src/p21-worldwise/App.tsx
@@ -1,3 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
+
 export default function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
