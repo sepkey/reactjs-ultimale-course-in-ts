0c11e4657d978a73c7285425821afa171ae22df0
Sepide Kia <sepidekia6990@gmail.com>
2023-07-22 20:34:31 +0330

    add redirect to cities and back in a declaretive way with Navigate

--- src/p21-worldwise/App.tsx
+++ src/p21-worldwise/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
@@ -51,10 +51,7 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList isLoading={isLoading} cities={cities} />}
-          />
+          <Route index element={<Navigate to="cities" replace />} />
           <Route
             path="cities"
             index
