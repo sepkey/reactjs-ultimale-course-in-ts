a97d82a47665b580689c43a9fd341a16d014fda0
Sepide Kia <sepidekia6990@gmail.com>
2023-07-26 21:25:44 +0330

    make a reusable component for back buttons

---
+++ src/p23-worldwise/components/ButtonBack.tsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+
+export default function ButtonBack() {
+  const navigate = useNavigate();
+
+  return (
+    <Button type="back" onClick={() => navigate(-1)}>
+      &larr; Back
+    </Button>
+  );
+}
