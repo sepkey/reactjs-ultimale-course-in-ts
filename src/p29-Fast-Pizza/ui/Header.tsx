cd10ca0002504f2e1d7edfddaf0fcc2abfa563d2
Sepide Kia <sepidekia6990@gmail.com>
2023-08-15 21:54:41 +0330

    add spacing and font

--- src/p29-Fast-Pizza/ui/Header.tsx
+++ src/p29-Fast-Pizza/ui/Header.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <div>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <div className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Sepide</p>
+      <Username />
     </div>
   );
 }
