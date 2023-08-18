e3c356ec3fa6af32fa52a03ccefe46ebc12e93fd
Sepide Kia <sepidekia6990@gmail.com>
2023-08-18 11:08:37 +0330

    finish order and order item styling and the whole project

--- src/p29-Fast-Pizza/features/order/OrderItem.tsx
+++ src/p29-Fast-Pizza/features/order/OrderItem.tsx
@@ -3,20 +3,20 @@ import { formatCurrency } from "../../utils/helpers";
 
 type Props = {
   item: Item;
-  isLoadingIngredients: boolean;
-  ingredients: string[];
+  isLoadingIngredients?: boolean;
+  ingredients?: string[];
 };
 
-function OrderItem({ item, isLoadingIngredients, ingredients }: Props) {
+function OrderItem({ item }: Props) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li>
-      <div>
+    <li className="py-3">
+      <div className="flex items-center justify-between gap-4 text-sm">
         <p>
-          <span>{quantity}&times;</span> {name}
+          <span className="font-bold">{quantity}&times;</span> {name}
         </p>
-        <p>{formatCurrency(totalPrice)}</p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
     </li>
   );
