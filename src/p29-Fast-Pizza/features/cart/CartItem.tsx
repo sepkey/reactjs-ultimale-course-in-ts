bd2204d34069aa7a1b1d374f9973249be4b017e9
Sepide Kia <sepidekia6990@gmail.com>
2023-08-18 09:32:41 +0330

    finish the cart and cart items style and add a new type to button

--- src/p29-Fast-Pizza/features/cart/CartItem.tsx
+++ src/p29-Fast-Pizza/features/cart/CartItem.tsx
@@ -1,4 +1,5 @@
 import { Item } from "../../models/models";
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 
 type Props = {
@@ -9,12 +10,13 @@ function CartItem({ item }: Props) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li>
-      <p>
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
+      <div className="flex items-center sm:gap-6">
+        <p className="text-sm font-semibold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
