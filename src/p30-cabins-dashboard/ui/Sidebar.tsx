08e4bae5fc3a5ef7add5e87d2bcc914048e923ca
Sepide Kia <sepidekia6990@gmail.com>
2023-09-01 15:45:54 +0330

    build the layout

---
+++ src/p30-cabins-dashboard/ui/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border: 1px solid var(--color-grey-100);
+  grid-row: 1/-1;
+`;
+export default function Sidebar() {
+  return <StyledSidebar>Sidebar</StyledSidebar>;
+}
