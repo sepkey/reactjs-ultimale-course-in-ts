44e04a5687f21f96c94455838fa72a78f1140faa
Sepide Kia <sepidekia6990@gmail.com>
2023-09-17 23:16:33 +0330

    add filter feature to cabiins table and make it reusable

--- src/p33-cabins-dashboard/features/cabins/CabinTable.tsx
+++ src/p33-cabins-dashboard/features/cabins/CabinTable.tsx
@@ -4,12 +4,29 @@ import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
 import { IFetchedCabin } from "../../cabins.interface";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
+type Filters = "all" | "no-discount" | "with-discount";
 
 export default function CabinTable() {
   const { cabins, isLoading } = useCabins();
-
+  const [searchParams] = useSearchParams();
   if (isLoading) return <Spinner />;
 
+  const filterValue = (searchParams.get("discount") as Filters) || "all";
+  let filteredCabins: IFetchedCabin[];
+  if (filterValue === "all") {
+    filteredCabins = cabins as IFetchedCabin[];
+  }
+  if (filterValue === "no-discount") {
+    filteredCabins = cabins?.filter(
+      (item) => item.discount === 0,
+    ) as IFetchedCabin[];
+  }
+  if (filterValue === "with-discount") {
+    filteredCabins = cabins?.filter(
+      (item) => item.discount! > 0,
+    ) as IFetchedCabin[];
+  }
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -21,7 +38,7 @@ export default function CabinTable() {
           <div>Discount</div>
         </Table.Header>
         <Table.Body
-          data={cabins as IFetchedCabin[]}
+          data={filteredCabins!}
           render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
         />
       </Table>
---
+++ src/p33-cabins-dashboard/ui/Filter.tsx
@@ -0,0 +1,69 @@
+import { useSearchParams } from "react-router-dom";
+import styled, { css } from "styled-components";
+
+const StyledFilter = styled.div`
+  border: 1px solid var(--color-grey-100);
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+  border-radius: var(--border-radius-sm);
+  padding: 0.4rem;
+  display: flex;
+  gap: 0.4rem;
+`;
+
+type FilterButtonProps = {
+  active: boolean;
+};
+
+const FilterButton = styled.button<FilterButtonProps>`
+  background-color: var(--color-grey-0);
+  border: none;
+
+  ${(props) =>
+    props.active &&
+    css`
+      background-color: var(--color-brand-600);
+      color: var(--color-brand-50);
+    `}
+
+  border-radius: var(--border-radius-sm);
+  font-weight: 500;
+  font-size: 1.4rem;
+  /* To give the same height as select */
+  padding: 0.44rem 0.8rem;
+  transition: all 0.3s;
+
+  &:hover:not(:disabled) {
+    background-color: var(--color-brand-600);
+    color: var(--color-brand-50);
+  }
+`;
+type Props = {
+  options: FilterType[];
+  filterField: string;
+};
+type FilterType = { value: string; label: string };
+
+export default function Filter({ options, filterField }: Props) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const currentFilter = searchParams.get(filterField) || options.at(0)?.value;
+
+  function handleClick(value: string) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+        <FilterButton
+          key={option.value}
+          onClick={() => handleClick(option.value)}
+          active={option.value === currentFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
