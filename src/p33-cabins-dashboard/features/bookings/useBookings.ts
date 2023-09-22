a152c3f90a01352996cb406507858beb47e6a9c6
Sepide Kia <sepidekia6990@gmail.com>
2023-09-22 23:02:44 +0330

    add prefetch feature to the pagination

--- src/p33-cabins-dashboard/features/bookings/useBookings.ts
+++ src/p33-cabins-dashboard/features/bookings/useBookings.ts
@@ -1,9 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export default function useBookings() {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   // Filter
   const filterValue = searchParams.get("status");
@@ -17,12 +19,27 @@ export default function useBookings() {
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
-  //Pagination
+  // Pagination
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
   const { data: { data: bookings, count } = {}, isLoading } = useQuery({
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  // Pre-fetching
+  const pageCount = Math.ceil(count! / PAGE_SIZE);
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+
   return { bookings, isLoading, count };
 }
