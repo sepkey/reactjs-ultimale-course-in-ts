44e23ffa62a4ccbf3e32571230835b073b57a361
Sepide Kia <sepidekia6990@gmail.com>
2023-09-25 23:02:45 +0330

    add authorization and protected route feature

--- src/p33-cabins-dashboard/services/apiAuth.ts
+++ src/p33-cabins-dashboard/services/apiAuth.ts
@@ -10,3 +10,14 @@ export async function login({ email, password }: LoginProps) {
   if (error) throw new Error(error.message);
   return data;
 }
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+  console.log(data);
+
+  if (error) throw new Error(error.message);
+  return data?.user;
+}
