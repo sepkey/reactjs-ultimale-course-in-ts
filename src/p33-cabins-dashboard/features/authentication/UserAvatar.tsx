2d9ad8af38d71759dce81bfb4f021e95b2d7d4a5
Sepide Kia <sepidekia6990@gmail.com>
2023-09-27 22:17:52 +0330

    create header with avatar

--- src/p33-cabins-dashboard/features/authentication/UserAvatar.tsx
+++ src/p33-cabins-dashboard/features/authentication/UserAvatar.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import useUser from "./useUser";
 
 const StyledUserAvatar = styled.div`
   display: flex;
@@ -19,3 +20,16 @@ const Avatar = styled.img`
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
 `;
+
+export default function UserAvatar() {
+  const { user } = useUser();
+  return (
+    <StyledUserAvatar>
+      <Avatar
+        src={user?.user_metadata.avatar || "default-user.jpg"}
+        alt={`Avatar of ${user?.user_metadata.fullName || ""}`}
+      />
+      <span>{user?.user_metadata.fullName}</span>
+    </StyledUserAvatar>
+  );
+}
