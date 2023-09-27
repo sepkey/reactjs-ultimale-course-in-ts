5ac3bdc6cbefb639f227b1ef13c743d05d713c73
Sepide Kia <sepidekia6990@gmail.com>
2023-09-27 07:44:55 +0330

    implement submit function using useSubmit custom hook and active the email confirmation while creating user

--- src/p33-cabins-dashboard/features/authentication/SignupForm.tsx
+++ src/p33-cabins-dashboard/features/authentication/SignupForm.tsx
@@ -3,10 +3,11 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import useSignup from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 type FormValues = {
-  fullname: string;
+  fullName: string;
   email: string;
   password: string;
   passwordConfirm: string;
@@ -17,24 +18,35 @@ function SignupForm() {
     formState: { errors },
     getValues,
     handleSubmit,
+    reset,
   } = useForm<FormValues>();
+  const { isLoading, signup } = useSignup();
 
-  function onSubmint(data: FormValues) {
-    console.log(data);
+  function onSubmint({ fullName, email, password }: FormValues) {
+    signup(
+      { email, fullName, password },
+      {
+        onSettled: () => {
+          reset();
+        },
+      },
+    );
   }
 
   return (
     <Form onSubmit={handleSubmit(onSubmint)}>
-      <FormRow label="Full name" error={errors.fullname?.message}>
+      <FormRow label="Full name" error={errors.fullName?.message}>
         <Input
+          disabled={isLoading}
           type="text"
           id="fullName"
-          {...register("fullname", { required: "This field is required" })}
+          {...register("fullName", { required: "This field is required" })}
         />
       </FormRow>
 
       <FormRow label="Email address" error={errors.email?.message}>
         <Input
+          disabled={isLoading}
           type="email"
           id="email"
           {...register("email", {
@@ -52,6 +64,7 @@ function SignupForm() {
         error={errors.password?.message}
       >
         <Input
+          disabled={isLoading}
           type="password"
           id="password"
           {...register("password", {
@@ -66,6 +79,7 @@ function SignupForm() {
 
       <FormRow label="Repeat password" error={errors.passwordConfirm?.message}>
         <Input
+          disabled={isLoading}
           type="password"
           id="passwordConfirm"
           {...register("passwordConfirm", {
@@ -78,10 +92,10 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" disabled={isLoading}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>Create new user</Button>
       </FormRow>
     </Form>
   );
--- src/p33-cabins-dashboard/services/apiAuth.ts
+++ src/p33-cabins-dashboard/services/apiAuth.ts
@@ -1,5 +1,18 @@
 import supabase from "./supabase";
 
+export type SignupProps = { email: string; password: string; fullName: string };
+
+export async function signup({ email, fullName, password }: SignupProps) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: { data: { fullName, avatar: "" } },
+  });
+
+  if (error) throw new Error(error.message);
+  return data;
+}
+
 export type LoginProps = { email: string; password: string };
 
 export async function login({ email, password }: LoginProps) {
