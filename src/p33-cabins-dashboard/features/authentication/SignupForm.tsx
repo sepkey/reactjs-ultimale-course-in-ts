478cd7cdd95d26700f274e49f67c4d194bc8ead3
Sepide Kia <sepidekia6990@gmail.com>
2023-09-27 07:11:22 +0330

    create a form for creating user, and solve the validation of it-part1

---
+++ src/p33-cabins-dashboard/features/authentication/SignupForm.tsx
@@ -0,0 +1,90 @@
+import { useForm } from "react-hook-form";
+import Button from "../../ui/Button";
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+
+// Email regex: /\S+@\S+\.\S+/
+type FormValues = {
+  fullname: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+};
+function SignupForm() {
+  const {
+    register,
+    formState: { errors },
+    getValues,
+    handleSubmit,
+  } = useForm<FormValues>();
+
+  function onSubmint(data: FormValues) {
+    console.log(data);
+  }
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmint)}>
+      <FormRow label="Full name" error={errors.fullname?.message}>
+        <Input
+          type="text"
+          id="fullName"
+          {...register("fullname", { required: "This field is required" })}
+        />
+      </FormRow>
+
+      <FormRow label="Email address" error={errors.email?.message}>
+        <Input
+          type="email"
+          id="email"
+          {...register("email", {
+            required: "This field is required",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Please provide a valid email address",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Password (min 8 characters)"
+        error={errors.password?.message}
+      >
+        <Input
+          type="password"
+          id="password"
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 8,
+              message: "Password needs a minimum of 8 characters.",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Repeat password" error={errors.passwordConfirm?.message}>
+        <Input
+          type="password"
+          id="passwordConfirm"
+          {...register("passwordConfirm", {
+            required: "This field is required",
+            validate: (value) =>
+              value === getValues().password || "Passwords need to match",
+          })}
+        />
+      </FormRow>
+
+      <FormRow>
+        {/* type is an HTML attribute! */}
+        <Button variation="secondary" type="reset">
+          Cancel
+        </Button>
+        <Button>Create new user</Button>
+      </FormRow>
+    </Form>
+  );
+}
+
+export default SignupForm;
